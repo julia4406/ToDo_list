ae291e15d23286b331dc3f67ab1c7e1bb218d7ce
Julia Alexeeva <julia4406@gmail.com>
2025-01-31 12:55:50 +0200

    Created tag_list html page. Added page for creation new tag

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 
-from todo_app.views import IndexView
+from todo_app.views import HomePageView, TagListView, TagCreateView
 
 app_name="todo_app"
 
 urlpatterns = [
-    path("", IndexView.as_view(), name="index"),
+    path("", HomePageView.as_view(), name="index"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create")
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 
-from todo_app.models import Task
+from todo_app.forms import TagForm
+from todo_app.models import Task, Tag
 
 
-class IndexView(ListView):
+class HomePageView(ListView):
     model = Task
     template_name = "todo_app/index.html"
+    context_object_name = "tasks"
+
+
+class TagListView(ListView):
+    model = Tag
+    template_name = "todo_app/tag_list.html"
+    context_object_name = "tags"
+
+
+class TagCreateView(CreateView):
+    model = Tag
+    form_class = TagForm
+    success_url = reverse_lazy("todo_app:tag-list")
