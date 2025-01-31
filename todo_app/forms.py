41e601e2b4a429380f547fee8e8376e6f2ed94f1
Julia Alexeeva <julia4406@gmail.com>
2025-01-31 14:30:09 +0200

    Added functionality in todo-list: add/update/delete

--- todo_app/forms.py
+++ todo_app/forms.py
@@ -1,9 +1,29 @@
 from django import forms
 
-from todo_app.models import Tag
+from todo_app.models import Tag, Task
 
 
 class TagForm(forms.ModelForm):
     class Meta:
         model = Tag
         fields = ["name",]
+
+
+class TaskForm(forms.ModelForm):
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+    content = forms.CharField(
+        widget=forms.Textarea(attrs={'rows': 3, 'cols': 40}),
+        required=True
+    )
+    deadline = forms.DateTimeField(
+        widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        required=False
+    )
+
+    class Meta:
+        model = Task
+        fields = ["content", "deadline", "tags"]
--- todo_app/urls.py
+++ todo_app/urls.py
@@ -2,7 +2,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 
-from todo_app.views import HomePageView, TagListView, TagCreateView, TagUpdateView, TagDeleteView
+from todo_app.views import (
+    HomePageView,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView
+)
+
 
 app_name="todo_app"
 
@@ -11,5 +21,8 @@ urlpatterns = [
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
     path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
-    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete")
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
+    path("task/create/", TaskCreateView.as_view(), name="task-create"),
+    path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
