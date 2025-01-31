aa9ebba906bec47db08191c863359c3891dd7a38
Julia Alexeeva <julia4406@gmail.com>
2025-01-31 12:55:50 +0200

    Created tag_list html page. Added page for creation new tag

--- todo_app/models.py
+++ todo_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Task(models.Model):
     content = models.TextField()
-    create_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     deadline = models.DateTimeField(blank=True, null=True)
     is_done = models.BooleanField(default=False)
     tag = models.ManyToManyField("Tag", related_name="tags")
