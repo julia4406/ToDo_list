e88777e30d5039eb947b1fe57070d359e4f413e0
Julia Alexeeva <julia4406@gmail.com>
2025-01-31 13:21:22 +0200

    To tag-list page added update/delete items. Feature: delete function realized without confirmation deleting.

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -2,12 +2,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 
-from todo_app.views import HomePageView, TagListView, TagCreateView
+from todo_app.views import HomePageView, TagListView, TagCreateView, TagUpdateView, TagDeleteView
 
 app_name="todo_app"
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="index"),
     path("tags/", TagListView.as_view(), name="tag-list"),
-    path("tags/create/", TagCreateView.as_view(), name="tag-create")
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete")
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
